481cfb15db31a115f583fc2f40c0ab0f5abc78a7
geonwoomun <ansejrrhkd@naver.com>
2019-03-21 20:38:42 +0900

    Add Customer Delete  Module

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,43 +7,57 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host : conf.host,
+  user : conf.user,
+  password : conf.password,
+  port : conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
+
+const multer = require('multer');
+const upload = multer({dest: './upload'});
+
 app.get('/api/customers', (req,res) => {
-    res.send([
-            {
-            
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : '홍길동',
-            'birthday' : '941215',
-            'gender' : '남성',
-            'job' : '학생'
-            
-          },
-          
-          {
-            
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : '문건우',
-            'birthday' : '950601',
-            'gender' : '남성',
-            'job' : '프로그래머'
-            
-          },
-          
-          {
-            
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : '류기탁',
-            'birthday' : '960301',
-            'gender' : '남성',
-            'job' : '회장'
-            
-          }
-          
-          
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req,res) => {
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
+  let image = '/image/' + req.file.filename;
+
+  let NAME = req.body.NAME;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  
+  let params = [image, NAME, birthday, gender, job];
+  connection.query(sql,params,
+    (err,rows, fields) => {
+      res.send(rows);
+    })
 })
+app.delete('/api/customers/:id', (req, res) => {
+  let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+  let params = [req.params.id];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+    })
+}) 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
